c8a24517aa5605f3934b282545db125d056c5520
Bob Carroll <bob.carroll@alum.rit.edu>
2024-12-04 20:22:40 -0800

    add pypi metadata

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(name='dsock',
-      version='0.1',
+      version='0.5',
       description='File-based socket server',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Bob Carroll',
       author_email='bob.carroll@alum.rit.edu',
+      url='https://git.bobc.io/bobc/dsock',
       packages=find_packages(include=['dsock']),
       classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Environment :: No Input/Output (Daemon)',
         'Framework :: AsyncIO',
@@ -16,7 +22,7 @@ setup(name='dsock',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
-        'Environment :: Win32 (MS Windows)',
         'Programming Language :: Python :: 3',
         'Topic :: System :: Networking'])
